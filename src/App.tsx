600f8b2f34be360b8b96e54ca34348afb2e33627
DevNetoSantos <rasneto147@gmail.com>
2022-05-20 20:38:44 -0300

    Area 1 quase pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import style from './App.module.css';
 import imageLogo from './assets/images/powered.png';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weghtField, setWeightField] = useState<number>(0);
+
+  const buttonCalc = () => {
+    if(heightField && weghtField) {
+
+    } else {
+      alert('ERROR')
+    }
+  }
+
   return(
     <div className={style.main}>
       <header>
@@ -11,7 +24,26 @@ const App = () => {
       </header>
       <div className={style.container}>
         <div className={style.leftSide}>
-          ...
+          <h1>Calcule seu IMC</h1>
+          <p>
+          O índice de massa corporal é uma medida internacional usada para calcular se uma pessoa está no peso ideal.
+          </p>
+
+          <input 
+            type="number" 
+            placeholder='Digite Sua Altura.'
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+
+          <input 
+            type="number" 
+            placeholder='Digite Seu Peso.'
+            value={ weghtField > 0 ? weghtField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+          
+          <button onClick={buttonCalc}>Calcular</button>
         </div>
         <div className={style.rigthSide}>
           ...
