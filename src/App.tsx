3dc211ce7062601564e8067159cf83b5da5d48cf
DevNetoSantos <rasneto147@gmail.com>
2022-05-20 20:01:30 -0300

    começando header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import style from './App.module.css';
+import imageLogo from './assets/images/powered.png';
+
 const App = () => {
   return(
-    <div>
-      ...
+    <div className={style.main}>
+      <header>
+        <div className={style.headerContainer}>
+          <img src={imageLogo} alt="logo do site" width={150}/>
+        </div>
+      </header>
     </div>
   );
 }
