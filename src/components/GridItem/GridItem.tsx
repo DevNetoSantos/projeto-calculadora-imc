1dd51194b177a98800f3625074bef5cc18e42fd7
DevNetoSantos <rasneto147@gmail.com>
2022-05-21 15:34:18 -0300

    cores com cada modulo pronto

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,11 +1,15 @@
 import { level } from "../../helpers/imc";
+import style from './GridItem.module.css';
+import upImage from '../../assets/images/up.png';
+import downImage from '../../assets/images/down.png';
 
 type Props = {
     item: level;
-}
+};
+
 export const GridItem = ({item}: Props) => {
     return(
-        <div>
+        <div className={style.main} style={{backgroundColor: item.color}}>
             ...
         </div>
     );
--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -7,7 +7,7 @@ export type level = {
 }
 
 export const levels = [
-    { title: 'Magreza', color: '#96A3AbB', icon: 'dwon', imc: [0, 18.5]},
+    { title: 'Magreza', color: '#96A3AB', icon: 'dwon', imc: [0, 18.5]},
     { title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9]},
     { title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 30]},
     { title: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30.1, 99]}
