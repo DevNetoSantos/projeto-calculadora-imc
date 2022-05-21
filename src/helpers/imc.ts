9e6a450f889eb7f3c32249b0327dabd4391989f0
DevNetoSantos <rasneto147@gmail.com>
2022-05-20 21:31:37 -0300

    base calculo imc pronta

---
+++ src/helpers/imc.ts
@@ -0,0 +1,17 @@
+export type level = {
+    title: string;
+    color: string;
+    icon: 'down' | 'up';
+    imc: number[]
+}
+
+export const levels = [
+    { title: 'Magreza', color: '#96A3AbB', icon: 'dwon', imc: [0, 18.5]},
+    { tile: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9]},
+    { tiile: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 30]},
+    { tile: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30.1, 99]}
+];
+
+export const calImc = (height: number, weight: number) => {
+
+}
